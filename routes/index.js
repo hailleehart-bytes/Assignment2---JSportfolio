b6e124f2b6ea96f7f248af02d65b1070db409586
hailleehart-bytes <Haillee.gaineshart@ontariotechu.net>
2025-11-08 22:40:51 -0500

    Added final texts and images

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-// Home
 router.get('/', (req, res) => {
   res.render('index', { title: "Haillee's Portfolio" });
-});
-
-// About
-router.get('/about', (req, res) => {
-  res.render('about', { title: 'About Me' });
-});
-
-// Projects
-router.get('/projects', (req, res) => {
-  res.render('projects', { title: 'Projects' });
-});
-
-// Skills
-router.get('/skills', (req, res) => {
-  res.render('skills', { title: 'Skills' });
-});
-
-// Contact
-router.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
 });
 
 export default router;
